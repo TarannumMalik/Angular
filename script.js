4899182a656a9e320daea95cb97ad4576e33fcf5
TarannumMalik <tarannum.malik@somaiya.edu>
2023-09-28 19:15:42 +0530

    version1

---
+++ script.js
@@ -0,0 +1,20 @@
+var swiper = new swiper(".product-slider", {
+    loop: true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction:false,
+    },
+
+    breakpoints: {
+        "0": {
+            slidesPerView: 1,
+        },
+        "768": {
+            slidesPerView: 2,
+        },
+        "1020": {
+            slidesPerView: 3,
+        },
+    },
+});
